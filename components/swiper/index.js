0376df4a7c836534a8ddeb8edf5191de0aa4b547
drodata <drodata@foxmail.com>
2019-07-19 15:52:15 +0800

    add: my first custom component <dro-swiper>

---
+++ components/swiper/index.js
@@ -0,0 +1,33 @@
+Component({
+  options: {
+    styleIsolation: 'apply-shared'
+  },
+  /**
+   * Component properties
+   */
+  properties: {
+    items: Array,
+    autoplay: {
+      type: Boolean,
+      value: false
+    },
+    indicatorDots: {
+      type: Boolean,
+      value: true
+    }
+  },
+
+  /**
+   * Component initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Component methods
+   */
+  methods: {
+
+  }
+})
